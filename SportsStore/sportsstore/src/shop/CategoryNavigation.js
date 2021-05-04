0832715bc1e0d0895c2a1e4bcdc525a2b2a717fd
Michał <mwarioxin@gmail.com>
2021-05-04 22:10:14 +0200

    Aded basic display components and Data store connector

---
+++ SportsStore/sportsstore/src/shop/CategoryNavigation.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+//import { ToggleLink } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
+
+export class CategoryNavigation extends Component {
+  render() {
+    return (
+      <>
+        <ToggleLink to={this.props.baseUrl} exact={true}>
+          Wszystkie
+        </ToggleLink>
+        {this.props.categories &&
+          this.props.categories.map((cat) => (
+            <ToggleLink
+              key={cat}
+              to={`${this.props.baseUrl}/${cat.toLowerCase()}`}
+            >
+              {cat}
+            </ToggleLink>
+          ))}
+      </>
+    );
+  }
+}
